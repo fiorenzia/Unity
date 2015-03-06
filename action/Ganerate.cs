0cfcfe21c0d959e96165f2385958648c3606601e
Fiorenzia <mixxxx11111@gmail.com>
2015-03-06 20:04:31 +0900

    add Generate.cs: EnemyGenerater.
    fix
    Ruby.cs: add comment.

---
+++ action/Ganerate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ganerate : MonoBehaviour {
+	public GameObject myPrefub;
+	int generatetime=120;
+	int timecount=0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timecount++;
+		Vector3 offs=new Vector3(Mathf.Sin(Time.time*0.5f),0,0);
+		if(generatetime==timecount){
+		Instantiate (myPrefub,transform.position+offs,Quaternion.identity);
+		timecount=0;
+		}
+	}
+}
--- action/Ruby.cs
+++ action/Ruby.cs
@@ -11,23 +11,34 @@ public class Ruby : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update (){
-		Debug.Log(+dashtime);
+		//move
 		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (0.005f, 0, 0);
+			transform.Translate (0.01f, 0, 0);
 			dashtime++;
-			if(dashtime>60){
-			transform.Translate (0.05f, 0, 0);
+			if(dashtime>60){ //this timecount change dashmode
+			transform.Translate (0.08f, 0, 0);
 			}
 		}
 		if (Input.GetKey (KeyCode.A)) {
 			transform.Translate (-0.005f, 0, 0);
 			dashtime++;
-			if (dashtime > 60) {
+			if (dashtime > 60) { //this timecount change dashmode
 			transform.Translate (-0.05f, 0, 0);
 			}
 		}
+
+		//walk mode release key
 		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
 			dashtime=0;
+		}
+
+		if (Input.GetMouseButtonDown (0))//Left Button 
+		{
+			transform.Translate (0.5f, 0, 0); //test
+		}
+		if (Input.GetMouseButtonDown (1))//Right Button 
+		{
+			transform.Translate (1.5f, 0, 0); //test
 		}
 	}
 }
