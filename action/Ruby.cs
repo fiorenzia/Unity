49115f620f763e431b4c1da861e58ff3b56e4cc0
fiorenzia <hadesu_1192@yahoo.co.jp>
2015-03-07 22:14:19 +0900

    revise
    erase:
    int dashtime
    walk mode
    
    add:
    facing right and facing left
    
    always dash mode

--- action/Ruby.cs
+++ action/Ruby.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 using System.Collections;
 public class Ruby : MonoBehaviour
 {
-	int dashtime=0;
 	bool rightleft=false;
 
-	// Use this for initialization
+	//Use this for initialization
 	void Start ()
 	{
 	
@@ -19,26 +18,14 @@ public class Ruby : MonoBehaviour
 			transform.Translate (0.1f, 0, 0);
 			dashtime++;
 			rightleft=false; // right side
-//			if(dashtime>60)
-//			{ 
-//				transform.Translate (0.08f, 0, 0); //this timecount change dashmode
-//			}
+
 		}
 		if (Input.GetKey (KeyCode.A)) 
 		{
 			transform.Translate (-0.1f, 0, 0);
 			dashtime++;
 			rightleft=true; // left side
-//			if (dashtime > 60) 
-//			{	//this timecount change dashmode
-//				transform.Translate (-0.08f, 0, 0);
-//			}
 		}
-		//walk mode release key
-//		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) 
-//		{
-//			dashtime=0;
-//		}
 
 		if (Input.GetMouseButtonDown (0))//Left Button
 		{
