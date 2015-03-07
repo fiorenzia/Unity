c4fccf640965ec3f4ab3d4e56c94ed2c4929b4b4
fiorenzia <hadesu_1192@yahoo.co.jp>
2015-03-07 22:06:36 +0900

    Update Ruby.cs

--- action/Ruby.cs
+++ action/Ruby.cs
@@ -1,44 +1,56 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
-
-public class Ruby : MonoBehaviour 
+public class Ruby : MonoBehaviour
 {
 	int dashtime=0;
+	bool rightleft=false;
+
 	// Use this for initialization
-	void Start (){
+	void Start ()
+	{
 	
 	}
-	
 	// Update is called once per frame
-	void Update (){
+	void Update ()
+	{
 		//move
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (0.01f, 0, 0);
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate (0.1f, 0, 0);
 			dashtime++;
-			if(dashtime>60){ //this timecount change dashmode
-			transform.Translate (0.08f, 0, 0);
-			}
+			rightleft=false; // right side
+//			if(dashtime>60)
+//			{ 
+//				transform.Translate (0.08f, 0, 0); //this timecount change dashmode
+//			}
 		}
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (-0.005f, 0, 0);
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate (-0.1f, 0, 0);
 			dashtime++;
-			if (dashtime > 60) { //this timecount change dashmode
-			transform.Translate (-0.05f, 0, 0);
-			}
+			rightleft=true; // left side
+//			if (dashtime > 60) 
+//			{	//this timecount change dashmode
+//				transform.Translate (-0.08f, 0, 0);
+//			}
 		}
-
 		//walk mode release key
-		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
-			dashtime=0;
-		}
+//		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) 
+//		{
+//			dashtime=0;
+//		}
 
-		if (Input.GetMouseButtonDown (0))//Left Button 
+		if (Input.GetMouseButtonDown (0))//Left Button
 		{
-			transform.Translate (0.5f, 0, 0); //test
-		}
-		if (Input.GetMouseButtonDown (1))//Right Button 
-		{
-			transform.Translate (1.5f, 0, 0); //test
+			if(rightleft==false)
+			{
+				transform.Translate (0.5f, 0, 0); //test
+			}
+			else if(rightleft==true)
+			{
+				transform.Translate (-0.5f, 0, 0); //test
+				
+			}
 		}
 	}
 }
